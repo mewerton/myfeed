5d7d5c751cb45b289c37b0edfd3919608cd9aa77
Mewerton Melo <mewerton@gmail.com>
2022-10-22 18:07:56 -0300

    Add files via upload

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { PencilLine } from 'phosphor-react'
+import { Avatar } from './Avatar'
 
 import styles from './Sidebar.module.css'
 
@@ -10,7 +11,7 @@ export function Sidebar(){
             src="https://images.unsplash.com/photo-1625306796650-0545cf38f5f5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=40" 
             />
             <div className={styles.profile}>
-                <img className={styles.avatar} src="https://github.com/mewerton.png" />
+                <Avatar src="https://github.com/diego3g.png"/>
                 <strong>Mewerton Melo</strong>
                 <span>Web Developer</span>
             </div>
